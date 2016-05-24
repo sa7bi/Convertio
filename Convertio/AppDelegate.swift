9ad6e6d1af56ec6172fe3ae6143d550564ece918
Sahbi <sahbi21@gmail.com>
2016-05-24 20:07:07 +0200

    Initial Commit

---
+++ Convertio/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Convertio
+//
+//  Created by sahbi.alhajbel on 24.05.16.
+//  Copyright © 2016 sahbi.alhajbel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
